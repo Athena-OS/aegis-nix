55ab752acb3ee45971665b686646e0aae4f19df0
D3vil0p3r <vozaanthony@gmail.com>
2024-01-23 23:27:44 +0100

    Update users.rs

--- src/functions/users.rs
+++ src/functions/users.rs
@@ -3,15 +3,18 @@ use std::process::Command;
 
 pub fn new_user(username: &str, password: &str, do_hash_pass: bool) {
     let mut _password = String::new();
-
-    files_eval(
-        files::sed_file(
-            "/mnt/etc/nixos/configuration.nix",
-            "username =.*",
-            &(format!("username = \"{}\";", username)),
-        ),
-        "Set username",
-    );
+    let config_path = "/mnt/etc/nixos/configuration.nix";
+    
+    let user_line = format!("username = \"{}\";", username);
+    match replace_line_in_file(config_path, "username =", &user_line) {
+        Ok(_) => {
+            log::info!("Set username");
+        }
+        Err(e) => {
+            log::error!("Set username ERROR: {}", e);
+        }
+    }
+    
     if do_hash_pass {
         let hashed_pass = hash_pass(password).stdout;
         _password = String::from_utf8_lossy(&hashed_pass).into_owned();
@@ -19,14 +22,16 @@ pub fn new_user(username: &str, password: &str, do_hash_pass: bool) {
     else {
         _password = password.to_string();
     }
-    files_eval(
-        files::sed_file(
-            "/mnt/etc/nixos/configuration.nix",
-            "hashed =.*",
-            &(format!("hashed = \"{}\";", _password)),
-        ),
-        "Set password hash",
-    );
+    
+    let hash_line = format!("hashed = \"{}\";", _password);
+    match replace_line_in_file(config_path, "hashed =", &hash_line) {
+        Ok(_) => {
+            log::info!("Set user password hash");
+        }
+        Err(e) => {
+            log::error!("Set user password hash ERROR: {}", e);
+        }
+    }
 }
 
 pub fn hash_pass(password: &str) -> std::process::Output {
@@ -39,12 +44,15 @@ pub fn hash_pass(password: &str) -> std::process::Output {
 }
 
 pub fn root_pass(root_pass: &str) {
-    files_eval(
-        files::sed_file(
-            "/mnt/etc/nixos/configuration.nix",
-            "hashedRoot =.*",
-            &(format!("hashedRoot = \"{}\";", root_pass)),
-        ),
-        "Set root password hash",
-    );
+    let config_path = "/mnt/etc/nixos/configuration.nix";
+    
+    let hash_line = format!("hashedRoot = \"{}\";", root_pass);
+    match replace_line_in_file(config_path, "hashedRoot =", &hash_line) {
+        Ok(_) => {
+            log::info!("Set root password hash");
+        }
+        Err(e) => {
+            log::error!("Set root password hash ERROR: {}", e);
+        }
+    }
 }
